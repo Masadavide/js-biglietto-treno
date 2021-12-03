6231a2e5dd9a8b87d189dc14b789daf0d4e44a59
Masadavide <davide.masadm@gmail.com>
2021-12-03 15:49:07 +0100

    finito

--- myscript.js
+++ myscript.js
@@ -4,12 +4,14 @@ let userAge = prompt ('Inserisci la tua età')
 
 const costKm = (userKilometres * 0.21) //prezzo in base ai chilometri 
 
+console.log (costKm)
+
 if(userAge < 18){
     document.getElementById('costo').innerHTML =
-    (costKm - (costKm * 20 / 100));
+    (costKm - ((costKm / 100) * 20)).toFixed(2);
 } else if (userAge >= 65){
     document.getElementById('costo').innerHTML =
-    (costKm - (costKm * 40 / 100));
+    (costKm - ((costKm / 100) * 40)).toFixed(2);
 } else {
     document.getElementById('costo').innerHTML =
     (costKm);
